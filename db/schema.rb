7a2bb7cf687b449edcab3d905d0250919eff5c32
peck liu <xiaoyang20170204@gmail.com>
2017-05-29 16:57:35 +0800

    move sqlite3 to dev group & add pg to production group

--- db/schema.rb
+++ db/schema.rb
@@ -12,15 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170527045524) do
 
-  create_table "average_caches", force: :cascade do |t|
-    t.integer  "rater_id"
-    t.string   "rateable_type"
-    t.integer  "rateable_id"
-    t.float    "avg",           null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
     t.integer  "product_id"
@@ -117,29 +108,6 @@ ActiveRecord::Schema.define(version: 20170527045524) do
     t.integer  "category_id"
     t.integer  "purchase_quantity", default: 1
     t.string   "tag"
-  end
-
-  create_table "rates", force: :cascade do |t|
-    t.integer  "rater_id"
-    t.string   "rateable_type"
-    t.integer  "rateable_id"
-    t.float    "stars",         null: false
-    t.string   "dimension"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type"
-    t.index ["rater_id"], name: "index_rates_on_rater_id"
-  end
-
-  create_table "rating_caches", force: :cascade do |t|
-    t.string   "cacheable_type"
-    t.integer  "cacheable_id"
-    t.float    "avg",            null: false
-    t.integer  "qty",            null: false
-    t.string   "dimension"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
   end
 
   create_table "users", force: :cascade do |t|
